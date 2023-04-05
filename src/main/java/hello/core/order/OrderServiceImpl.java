272e9de184da67d8d484da3f99be6724c52e2c5f
CheolHee Kim <windowlife92@gmail.com>
2023-04-05 14:30:16 +0900

    230405_1430 commit

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -10,18 +10,12 @@ import org.springframework.stereotype.Component;
 @Component
 public class OrderServiceImpl implements OrderService{
 
-    private MemberRepository memberRepository;
-    private DiscountPolicy discountPolicy;
+    private final MemberRepository memberRepository;
+    private final DiscountPolicy discountPolicy;
     //    private final DiscountPolicy discountPolicy = new FixDiscountPolicy(); // 구현클래스에 의존
     //    private final DiscountPolicy discountPolicy = new RateDiscountPolicy(); // 구현클래스에 의존
 
     public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
-        this.memberRepository = memberRepository;
-        this.discountPolicy = discountPolicy;
-    }
-
-    @Autowired
-    public void init(MemberRepository memberRepository, DiscountPolicy discountPolicy){
         this.memberRepository = memberRepository;
         this.discountPolicy = discountPolicy;
     }
